998cb34a8cd2f6fbf18392c3ed2735e888838939
c332030 <c332030@gmail.com>
2022-07-10 21:08:45 +0800

    feat: sh -s

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-sh -c "curl -s https://github.com/cc332030/linux/raw/master/script/init-ssh/init-ssh-pre.sh | sh -- $*"
-sh -c "curl -s https://github.com/cc332030/linux/raw/master/script/init-ssh/init-ssh.sh | sh -- $*"
+sh -c "curl -s https://github.com/cc332030/linux/raw/master/script/init-ssh/init-ssh-pre.sh | sh -s -- $*"
+sh -c "curl -s https://github.com/cc332030/linux/raw/master/script/init-ssh/init-ssh.sh | sh -s -- $*"
 
-sh -c "curl -s https://github.com/cc332030/hugo/raw/master/script/hugo-build.sh | sh -- $*"
+sh -c "curl -s https://github.com/cc332030/hugo/raw/master/script/hugo-build.sh | sh -s -- $*"
 
 if [ -n "$COMMAND" ]
 then
