27dddd125c46a00f434be4038410d9591f0eb206
c332030 <c332030@gmail.com>
2022-07-10 22:04:32 +0800

    feat: curl_sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ set -e
 
 parameter=$*
 
-c_sh(){
+curl_sh(){
   sh -c "curl -sL $1 | sh -s -- $parameter"
 }
 
-c_sh https://github.com/cc332030/linux/raw/master/script/init-ssh/init-ssh-pre.sh
-c_sh https://github.com/cc332030/linux/raw/master/script/init-ssh/init-ssh.sh
+curl_sh https://github.com/cc332030/linux/raw/master/script/init-ssh/init-ssh-pre.sh
+curl_sh https://github.com/cc332030/linux/raw/master/script/init-ssh/init-ssh.sh
 
-c_sh https://github.com/cc332030/hugo/raw/master/script/hugo-build.sh
+curl_sh https://github.com/cc332030/hugo/raw/master/script/hugo-build.sh
 
 if [ -n "$COMMAND" ]
 then
