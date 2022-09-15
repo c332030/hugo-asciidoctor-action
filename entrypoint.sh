d18e9b3b14a2cfe6eb813462ee2897de23094ffc
c332030 <c332030@gmail.com>
2022-09-15 23:40:22 +0800

    feat: deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,11 @@ curl_sh https://github.com/cc332030/linux/raw/master/script/init-ssh/init-ssh.sh
 
 curl_sh https://github.com/cc332030/hugo/raw/master/script/hugo-build.sh
 
+if [ -n "$REMOTE_PATH" ]
+then
+  curl_sh https://github.com/cc332030/linux/raw/master/script/deploy/deploy.sh
+fi
+
 if [ -n "$COMMAND" ]
 then
   sh -c "${COMMAND}" || true
